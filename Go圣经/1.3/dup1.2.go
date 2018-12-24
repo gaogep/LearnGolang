a6ea8daa62fd0aea234703942e302d1597bd1d08
gaogep <zpf3981@qq.com>
2018-12-24 20:46:53 +0800

    tgl

---
+++ Go圣经/1.3/dup1.2.go
@@ -0,0 +1,34 @@
+// 打印输入中多次出现的行的个数和文本
+// 它从Stdin或指定的文件列表读取
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
